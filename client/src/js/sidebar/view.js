7ac4909433828cd6cc1a73e7407d080fe669ef4d
연 <mogoo22@naver.com>
2022-04-06 21:36:12 +0900

    feat: sidebar 액션 리스트 히든레이어 영역을 애니메이션 효과와 함께 숨김,보임 기능 구현 Related to: #17

--- client/src/js/sidebar/view.js
+++ client/src/js/sidebar/view.js
@@ -87,8 +87,8 @@ const render = (parent) => {
 const toggleSidebar = () => {
   const sidebar = $("aside");
   const sidebarMenuBtn = $(".sidebar__menu-button img");
+  sidebarMenuBtn.src = sidebar.classList.contains("show") ? "./svg/icon-menu.svg" : "./svg/icon-delete.svg";
   sidebar.classList.toggle("show");
-  sidebarMenuBtn.src = sidebar.classList.contains("show") ? "./svg/icon-delete.svg" : "./svg/icon-menu.svg";
 };
 
 const bindModel = () => {
