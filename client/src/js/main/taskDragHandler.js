3d5f56d92255de79543e01fd0eb54b103425e2d0
Hemudi <ksum1205@naver.com>
2022-04-08 16:25:00 +0900

    feat: drag and drop 기능 구현
    * 선택 된 origin task item , 드래그 되고 있는 task item, 이동 된 task item 3개의 요소를 활용

--- client/src/js/main/taskDragHandler.js
+++ client/src/js/main/taskDragHandler.js
@@ -1,18 +1,32 @@
 import { iconDelete } from "../constants/imagePath.js";
+import { $$ } from "../utils/utils.js";
+
+const taskItemClassName = "column__task--item";
+const taskListClassName = "column__task--list";
+
+const taskCreator = (origin) => {
+  const task = {
+    origin: origin,
+    copy: createCopyTask(origin),
+    moved: createCopyTask(origin, true),
+  };
+
+  return (type = null) => {
+    return type ? task[type] : task;
+  };
+};
 
 export const setTaskDragEvent = () => {
-  const taskList = [...document.querySelectorAll(".column__task--list")];
+  const taskList = [...$$(`.${taskListClassName}`)];
   taskList.forEach((taskList) => {
     const taskItems = [...taskList.children];
     taskItems.forEach(setMouseEvent);
   });
 };
 
-const setMouseEvent = (element) => {
-  element.addEventListener("mousedown", mouseDownHandler);
+export const setMouseEvent = (taskItem) => {
+  taskItem.addEventListener("mousedown", mouseDownHandler);
 };
-
-const isDeleteButton = (target) => target.classList.contains("column__task--delete-button");
 
 const mouseDownHandler = (event) => {
   const target = event.target;
@@ -22,23 +36,42 @@ const mouseDownHandler = (event) => {
   }
 
   const taskElement = target.closest("li");
-  const copyTaskElement = createCopyTask(taskElement);
+  const getTaskObject = taskCreator(taskElement);
+  const copyTaskElement = getTaskObject("copy");
+
   document.body.appendChild(copyTaskElement);
 
   const moveAt = createMoveAtFunc(event, taskElement, copyTaskElement);
   moveAt(event.pageX, event.pageY);
-  setDocumentEvent(moveAt);
+  setDocumentEvent(moveAt, getTaskObject);
+  taskElement.classList.add("blur");
 };
 
-const createCopyTask = (taskElement) => {
+const isDeleteButton = (target) => target.classList.contains("column__task--delete-button");
+
+const createMoveAtFunc = (event, originElement, copyElement) => {
+  const { shiftX, shiftY } = getShiftCoordinate(event, originElement);
+  return (pageX, pageY) => {
+    copyElement.style.left = pageX - shiftX + "px";
+    copyElement.style.top = pageY - shiftY + "px";
+  };
+};
+
+const createCopyTask = (taskElement, isMoved = false) => {
   const [sectionElement] = [...taskElement.children];
   const copyTaskElement = document.createElement("li");
   copyTaskElement.style.width = "308px";
   copyTaskElement.ondragstart = () => false;
   copyTaskElement.dataset.index = taskElement.dataset.index;
-  copyTaskElement.classList.add("column__task--item", "dragging");
+  copyTaskElement.classList.add(taskItemClassName);
   copyTaskElement.innerHTML = createTaskHTML([...sectionElement.children]);
   copyTaskElement.addEventListener("mouseup", mouseUpHandler);
+
+  if (!isMoved) {
+    copyTaskElement.classList.add("dragging");
+  }
+
+  setMouseEvent(copyTaskElement);
   return copyTaskElement;
 };
 
@@ -61,26 +94,60 @@ const getShiftCoordinate = (event, taskElement) => {
   return { shiftX: shiftX, shiftY: shiftY };
 };
 
-const createMoveAtFunc = (event, originElement, copyElement) => {
-  const { shiftX, shiftY } = getShiftCoordinate(event, originElement);
-  return (pageX, pageY) => {
-    copyElement.style.left = pageX - shiftX + "px";
-    copyElement.style.top = pageY - shiftY + "px";
-  };
-};
-
 const removeDocumentEvent = () => {
   document.removeEventListener("mousemove", mouseMoveHandler);
 };
 
-const setDocumentEvent = (moveAtFunc) => {
+const setDocumentEvent = (moveAtFunc, getTaskObject) => {
   document.moveAt = moveAtFunc;
+  document.getTaskObject = getTaskObject;
   document.addEventListener("mousemove", mouseMoveHandler);
 };
 
 const mouseMoveHandler = (event) => {
   const target = event.currentTarget;
+  const copyElement = target.getTaskObject("copy");
   target.moveAt(event.pageX, event.pageY);
+  const element = getElementByCoordinate(copyElement, { x: event.clientX, y: event.clientY });
+  if (element === null) {
+    copyElement.dispatchEvent(new Event("mouseup"));
+    return;
+  }
+
+  const newTaskList = element.closest(`.${taskListClassName}`);
+  if (newTaskList !== null) {
+    setTaskList(newTaskList);
+  }
+};
+
+const getElementByCoordinate = (element, { x, y }) => {
+  element.classList.add("hidden");
+  const belowElement = document.elementFromPoint(x, y);
+  element.classList.remove("hidden");
+  return belowElement;
+};
+
+let movedTaskList = null;
+
+const setTaskList = (newList) => {
+  const taskObject = document.getTaskObject();
+  const originList = taskObject["origin"].closest(`.${taskListClassName}`);
+  const isNewList = originList !== newList;
+
+  if (movedTaskList === newList) {
+    return;
+  }
+
+  if (movedTaskList !== originList && movedTaskList !== null) {
+    movedTaskList.removeChild(taskObject["moved"]);
+  }
+
+  movedTaskList = newList;
+
+  if (isNewList) {
+    movedTaskList.appendChild(taskObject["moved"]);
+    return;
+  }
 };
 
 const mouseUpHandler = (event) => {
@@ -91,7 +158,22 @@ const mouseUpHandler = (event) => {
   }
 
   const taskElement = target.closest("li");
-  document.removeEventListener("mousemove", mouseMoveHandler);
+  removeDocumentEvent();
   taskElement.removeEventListener("mouseup", mouseUpHandler);
   taskElement.remove();
+
+  if (isMoved(document.getTaskObject("origin"), document.getTaskObject("moved"))) {
+    document.getTaskObject("origin").remove();
+  } else {
+    document.getTaskObject("origin").classList.remove("blur");
+  }
+
+  movedTaskList = null;
+  delete document.getTaskObject;
+};
+
+const isMoved = (element, movedElement) => {
+  const taskList = element.closest(`.${taskListClassName}`);
+  const movedList = movedElement.closest(`.${taskListClassName}`);
+  return movedList !== null && taskList !== movedList;
 };
