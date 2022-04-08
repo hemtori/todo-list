2cdc21e1f56e38de5b48c473da8e803860b5bc93
연 <mogoo22@naver.com>
2022-04-08 20:02:03 +0900

    design: icon-Delete->icon-delete

--- client/src/js/constants/imagePath.js
+++ client/src/js/constants/imagePath.js
@@ -1,4 +1,4 @@
 export const iconMenu = "./client/public/svg/icon-menu.svg";
 export const iconAdd = "./client/public/svg/icon-add.svg";
-export const iconDelete = "./client/public/svg/icon-Delete.svg";
+export const iconDelete = "./client/public/svg/icon-delete.svg";
 export const profileImage = "./client/public/svg/profile.svg";
